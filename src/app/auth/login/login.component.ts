83a3589be6458280093952ce04ba9df00c9fea04
akanshmahendra <akki619@icloud.com>
2019-02-16 21:26:48 +0530

    Added ui state to app

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Subscription, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { AuthService } from '../auth.service';
-import { UIService } from '../../shared/ui.service';
-import * as fromApp from '../../app.reducer';
-import { map } from 'rxjs/operators';
+import * as fromRoot from '../../app.reducer';
 
 @Component({
   selector: 'app-login',
@@ -17,10 +15,10 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   isLoading$ : Observable<boolean>;
 
-  constructor(private authService: AuthService, private store : Store<{ ui : fromApp.State }>) {}
+  constructor(private authService: AuthService, private store : Store<fromRoot.State>) {}
 
   ngOnInit() {
-    this.isLoading$ = this.store.pipe(map(state => state.ui.isLoading));
+    this.isLoading$ = this.store.select(fromRoot.getIsLoading);
     // this.loadingSubs = this.uiService.loadingStateChanged.subscribe(isLoading => {
     //   this.isLoading = isLoading;
     // });
